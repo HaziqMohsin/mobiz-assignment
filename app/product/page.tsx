4ad97b5a7242caa54cbb576dd3ac1cd6d379c23d
HaziqMohsin <myhaziq3@gmail.com>
2023-06-18 21:12:29 +0800

    [Update] table product pagination, responsive using SWR

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -6,9 +6,9 @@ type Props = {};
 
 const page = (props: Props) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 overflow-x-hidden">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 overflow-hidden">
       <Link href="/product/create">
-        <button className="px-4 py-2 bg-blue-500 rounded-md">
+        <button className="px-4 py-2 bg-blue-500 rounded-md flex justify-end text-white">
           Add product
         </button>
       </Link>
--- components/TableProduct.tsx
+++ components/TableProduct.tsx
@@ -20,14 +20,14 @@ const TableProduct = (props: Props) => {
 
   const uiTd = "px-6 py-4";
 
+  const [skip, setSkip] = useState(0);
+  const [limit, setLimit] = useState(10);
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error, isLoading } = useSWR(
-    "https://dummyjson.com/products ",
+    `https://dummyjson.com/products?limit=${limit}&skip=${skip} `,
     fetcher
   );
-
-  const [skip, setSkip] = useState("");
-  const [limit, setLimit] = useState("");
 
   console.log(data);
 
@@ -35,52 +35,75 @@ const TableProduct = (props: Props) => {
     console.log(id);
   };
 
+  const handlePrevious = () => {
+    setSkip(skip - limit);
+  };
+
+  const handleNext = () => {
+    setSkip(skip + limit);
+  };
+
+  console.log(skip, limit);
+
   return (
-    <div className="relative overflow-x-hidden my-4">
-      <table className="table-auto overflow-x-auto">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            {th.map((v, i) => {
+    <>
+      <div className="relative overflow-x-auto my-4 w-full">
+        <table className="overflow-x-auto w-full">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              {th.map((v, i) => {
+                return (
+                  <th scope="col" className="px-6 py-3" key={i}>
+                    {v}
+                  </th>
+                );
+              })}
+            </tr>
+          </thead>
+          <tbody>
+            {data?.products.map((v, i) => {
               return (
-                <th scope="col" className="px-6 py-3" key={i}>
-                  {v}
-                </th>
+                <tr
+                  key={i}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-white"
+                >
+                  <td className={uiTd}>{v.title}</td>
+                  <td className={`${uiTd}`}>
+                    <p className="line-clamp-2">{v.description}</p>
+                  </td>
+                  <td className={uiTd}>{v.price}</td>
+                  <td className={uiTd}>{v.discountPercentage}</td>
+                  <td className={uiTd}>{v.rating}</td>
+                  <td className={uiTd}>{v.stock}</td>
+                  <td className={uiTd}>{v.brand}</td>
+                  <td className={uiTd}>{v.category}</td>
+                  <td className={uiTd}>
+                    <button
+                      className="px-2 dark:text-white bg-red-600 rounded-md"
+                      onClick={() => handleDelete(v.id)}
+                    >
+                      x
+                    </button>
+                  </td>
+                </tr>
               );
             })}
-          </tr>
-        </thead>
-        <tbody>
-          {data?.products.map((v, i) => {
-            return (
-              <tr
-                key={i}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-white"
-              >
-                <td className={uiTd}>{v.title}</td>
-                <td className={`${uiTd}`}>
-                  <p className="line-clamp-2">{v.description}</p>
-                </td>
-                <td className={uiTd}>{v.price}</td>
-                <td className={uiTd}>{v.discountPercentage}</td>
-                <td className={uiTd}>{v.rating}</td>
-                <td className={uiTd}>{v.stock}</td>
-                <td className={uiTd}>{v.brand}</td>
-                <td className={uiTd}>{v.category}</td>
-                <td className={uiTd}>
-                  <button
-                    className="px-2 dark:text-white bg-red-600 rounded-md"
-                    onClick={() => handleDelete(v.id)}
-                  >
-                    x
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
-          <tr></tr>
-        </tbody>
-      </table>
-    </div>
+            <tr></tr>
+          </tbody>
+        </table>
+      </div>
+      <div>
+        Showing {limit + skip} of {data?.total}
+      </div>
+      <div className="flex gap-3 items-end my-4">
+        {skip + limit > 10 ? (
+          <button onClick={() => handlePrevious()}>Previous</button>
+        ) : (
+          ""
+        )}
+        <button onClick={() => handleNext()}>Next</button>
+      </div>
+    </>
   );
 };
 
