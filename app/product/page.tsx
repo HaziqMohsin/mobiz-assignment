3fd7cc8ce697ab4a16e5a03104b43cee08d485a7
HaziqMohsin <myhaziq3@gmail.com>
2023-06-18 20:33:22 +0800

    [update] table product

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Link from "next/link";
+import TableProduct from "@/components/TableProduct";
 
 type Props = {};
 
 const page = (props: Props) => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div>this Product</div>
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 overflow-x-hidden">
       <Link href="/product/create">
         <button className="px-4 py-2 bg-blue-500 rounded-md">
           Add product
         </button>
       </Link>
+      <TableProduct />
     </main>
   );
 };
---
+++ components/TableProduct.tsx
@@ -0,0 +1,87 @@
+"use client";
+
+import React, { useState } from "react";
+import useSWR from "swr";
+
+type Props = {};
+
+const TableProduct = (props: Props) => {
+  const th = [
+    "Title",
+    "Description",
+    "Price",
+    "Discount",
+    "Rating",
+    "Stock",
+    "Brand",
+    "Category",
+    "Action",
+  ];
+
+  const uiTd = "px-6 py-4";
+
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "https://dummyjson.com/products ",
+    fetcher
+  );
+
+  const [skip, setSkip] = useState("");
+  const [limit, setLimit] = useState("");
+
+  console.log(data);
+
+  const handleDelete = (id) => {
+    console.log(id);
+  };
+
+  return (
+    <div className="relative overflow-x-hidden my-4">
+      <table className="table-auto overflow-x-auto">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            {th.map((v, i) => {
+              return (
+                <th scope="col" className="px-6 py-3" key={i}>
+                  {v}
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {data?.products.map((v, i) => {
+            return (
+              <tr
+                key={i}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 dark:text-white"
+              >
+                <td className={uiTd}>{v.title}</td>
+                <td className={`${uiTd}`}>
+                  <p className="line-clamp-2">{v.description}</p>
+                </td>
+                <td className={uiTd}>{v.price}</td>
+                <td className={uiTd}>{v.discountPercentage}</td>
+                <td className={uiTd}>{v.rating}</td>
+                <td className={uiTd}>{v.stock}</td>
+                <td className={uiTd}>{v.brand}</td>
+                <td className={uiTd}>{v.category}</td>
+                <td className={uiTd}>
+                  <button
+                    className="px-2 dark:text-white bg-red-600 rounded-md"
+                    onClick={() => handleDelete(v.id)}
+                  >
+                    x
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+          <tr></tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableProduct;
