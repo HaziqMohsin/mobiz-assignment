d1660a9a075e87db5f6dca9a2e7a42dc6f3c423b
HaziqMohsin <myhaziq3@gmail.com>
2023-06-19 17:22:28 +0800

    [update] react-chartjs
    replacement for react-vis

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,14 @@
-import { authOptions } from "@/utils/authOptions";
-import { getServerSession } from "next-auth/next";
-import { redirect } from "next/navigation";
 import AverageRatingProducts from "@/components/Chart/AverageRatingProducts";
-// import CountProductByCategory from "@/components/Chart/CountProductByCategory";
+import CountProductByCategory2 from "@/components/Chart/CountProductByCategory2";
 
 export default async function Home() {
-  //   const session = await getServerSession(authOptions);
-
-  //   if (!session) {
-  //     redirect("/api/auth/signin");
-  //   }
-
   const res = await fetch("https://dummyjson.com/products?limit=100");
   const data = await res.json();
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      {/* <div>this is protected page</div> */}
       <AverageRatingProducts data={data.products} />
-      {/* <CountProductByCategory data={data.products} /> */}
+      <CountProductByCategory2 dataProduct={data.products} />
     </main>
   );
 }
---
+++ components/Chart/CountProductByCategory2.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+import { Product } from "./types";
+
+type Props = {
+  dataProduct: Product[];
+};
+
+const CountProductByCategory2 = ({ dataProduct }: Props) => {
+  console.log(dataProduct);
+
+  const labelProduct = dataProduct.reduce((label: any, product) => {
+    const { category } = product;
+
+    if (label[category]) {
+      label[category]++;
+    } else {
+      label[category] = 1;
+    }
+    return label;
+  }, {});
+
+  console.log(labelProduct);
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top" as const,
+      },
+      title: {
+        display: true,
+        text: "Count product by category",
+      },
+    },
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const data = {
+    labels: Object.keys(labelProduct),
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: Object.values(labelProduct),
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <div className="my-4">
+      <Bar options={options} data={data} height={300} />
+    </div>
+  );
+};
+
+export default CountProductByCategory2;
